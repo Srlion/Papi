264daa9a22ac2326c0fc2b337d7cb000ce99bbff
Srlion <srlion32@gmail.com>
2025-11-18 20:32:57 +0200

    feat: add semi Lyn and SAM support

--- papi.lua
+++ papi.lua
@@ -1,11 +1,22 @@
 local Papi = {}
 
 function Papi.AddPermission(name, min_access, category)
-
+    assert(type(name) == "string", "Permission name must be a string")
+    assert(type(min_access) == "string", "Minimum access level must be a string")
+    assert(category == nil or type(category) == "string", "Category must be a string or nil")
+    if Lyn then
+        Lyn.Permission.Add(name, category or "Papi", min_access)
+    elseif sam then
+        sam.permissions.add(name, category or "Papi", min_access)
+    end
 end
 
 function Papi.GetPermissions()
-
+    if Lyn then
+        error("TODO")
+    elseif sam then
+        return sam.permissions.get()
+    end
 end
 
 function Papi.PlayerHasPermission(ply, perm_name)
