c2250ffae07f728de1c9f992f57c0098114b125c
MathsCalculator <arnomathei@gmail.com>
2025-11-18 19:06:26 +0100

    feat: initial functions

---
+++ papi.lua
@@ -0,0 +1,31 @@
+function Papi.AddPermission()
+
+end
+
+function Papi.GetPermissions()
+
+end
+
+function Papi.PlayerHasPermission()
+
+end
+
+function Papi.GetPlayersWithPermission()
+
+end
+
+function Papi.GetPlayerRoles()
+
+end
+
+function Papi.PlayerHasRole()
+
+end
+
+function Papi.SteamIDHasPermission()
+
+end
+
+function Papi.GetRoles()
+
+end
