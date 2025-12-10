a189ea95d0ea44d61094839618894f04e0243f91
Srlion <srlion32@gmail.com>
2025-12-10 18:07:43 +0200

    refactor: rewrite how queue functions work because it would still error if called too early

--- src/main.lua
+++ src/main.lua
@@ -20,16 +20,28 @@ local function check_load()
     end
 end
 
-local function queue(func, ...)
+local function queue(key, ...)
     Papi.Load()
     if not Papi.Loaded then
         local args, n = { ... }, select("#", ...)
         timer.Simple(0, function()
-            func(unpack(args, 1, n))
+            Papi.API[key](unpack(args, 1, n))
         end)
         return
     end
-    func(...)
+    Papi.API[key](...)
+end
+
+local function queue_cmd(key, ...)
+    Papi.Load()
+    if not Papi.Loaded then
+        local args, n = { ... }, select("#", ...)
+        timer.Simple(0, function()
+            Papi.API.Commands[key](unpack(args, 1, n))
+        end)
+        return
+    end
+    Papi.API.Commands[key](...)
 end
 
 function Papi.GetActiveAdminMod()
@@ -41,8 +53,7 @@ function Papi.AddPermission(name, min_access, category)
     assert(type(name) == "string", "Permission name must be a string")
     assert(type(min_access) == "string", "Minimum access level must be a string")
     assert(category == nil or type(category) == "string", "Category must be a string or nil")
-
-    queue(Papi.API.AddPermission, name, min_access, category or "Papi")
+    queue("AddPermission", name, min_access, category or "Papi")
 end
 
 function Papi.GetPermissions()
@@ -72,49 +83,49 @@ end
 
 function Papi.OnRoleChanges(identifier, func)
     assert(func == nil or type(func) == "function", "func must be a function or nil")
-    queue(Papi.API.OnRoleChanges, identifier, func)
+    queue("OnRoleChanges", identifier, func)
 end
 
 function Papi.IsSteamid64Banned(steamid64, callback)
     assert(type(callback) == "function", "callback must be a function")
-    queue(Papi.API.IsSteamid64Banned, steamid64, callback)
+    queue("IsSteamid64Banned", steamid64, callback)
 end
 
 function Papi.Commands.Kick(ply, reason)
-    queue(Papi.API.Commands.Kick, ply, reason)
+    queue_cmd("Kick", ply, reason)
 end
 
 function Papi.Commands.BanID64(steamid64, length, reason)
-    queue(Papi.API.Commands.BanID64, steamid64, length, reason)
+    queue_cmd("BanID64", steamid64, length, reason)
 end
 
 function Papi.Commands.Ban(ply, length, reason)
-    queue(Papi.API.Commands.Ban, ply, length, reason)
+    queue_cmd("Ban", ply, length, reason)
 end
 
 function Papi.Commands.UnbanID64(steamid64)
-    queue(Papi.API.Commands.UnbanID64, steamid64)
+    queue_cmd("UnbanID64", steamid64)
 end
 
 function Papi.Commands.Freeze(ply)
-    queue(Papi.API.Commands.Freeze, ply)
+    queue_cmd("Freeze", ply)
 end
 
 function Papi.Commands.Unfreeze(ply)
-    queue(Papi.API.Commands.Unfreeze, ply)
+    queue_cmd("Unfreeze", ply)
 end
 
 if CLIENT then
     function Papi.Commands.Goto(ply)
-        queue(Papi.API.Commands.Goto, ply)
+        queue_cmd("Goto", ply)
     end
 
     function Papi.Commands.Bring(ply)
-        queue(Papi.API.Commands.Bring, ply)
+        queue_cmd("Bring", ply)
     end
 
     function Papi.Commands.Return(ply)
-        queue(Papi.API.Commands.Return, ply)
+        queue_cmd("Return", ply)
     end
 end
 
@@ -142,11 +153,13 @@ function Papi.Load()
     MsgC(Color(168, 95, 183), "[Papi]", Color(255, 255, 255), " No supported admin mod found! Papi will not function!\n")
 end
 
+-- This function is used inside build.py to add admin mod loaders
 ---@diagnostic disable-next-line: unused-function, unused-local
 local function Add(loader)
     table.insert(ADMIN_MODS, loader)
 end
 
+-- Replaced by build.py --
 --[[ Admin Mod Loaders ]] --
 
 Papi.Load()
